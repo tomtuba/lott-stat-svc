83f8601b628ecb708e087fb6db74327727786a73
tomtuba@gmail.com <tomtuba@gmail.com>
2016-03-31 00:18:07 -0400

    adding json support

--- src/main/java/com/lottstat/webservice/StateService.java
+++ src/main/java/com/lottstat/webservice/StateService.java
@@ -13,25 +13,11 @@ public class StateService {
 	@GET
 	@Path("{id}")
 	public Response getStateByPostalCode(@PathParam("id") String id) {
-		System.out.println("id: " + id);
 		
 		State state = State.valueOfAbbreviation(id);
 		
 		return Response.status(200).entity("getStateByPostalCode is called, id : " + id + ", state: " + state.name())
 				.build();
-	}
-	
-	@GET
-	@Path("{year}/{month}/{day}")
-	public Response getUserHistory(@PathParam("year") int year,
-			@PathParam("month") int month, @PathParam("day") int day) {
-
-		String date = year + "/" + month + "/" + day;
-
-		return Response.status(200)
-				.entity("getUserHistory is called, year/month/day : " + date)
-				.build();
-
 	}
 	
 }
