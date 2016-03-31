6b8b9eb113a3fd617356d04cbcee502f79afd98d
tomtuba@gmail.com <tomtuba@gmail.com>
2016-03-30 23:04:12 -0400

    completing initial service

--- src/main/java/com/lottstat/webservice/StateService.java
+++ src/main/java/com/lottstat/webservice/StateService.java
@@ -5,15 +5,33 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Response;
 
+import com.lottstat.entity.State;
+
 @Path("/states")
 public class StateService {
 
 	@GET
-	@Path("id")
+	@Path("{id}")
 	public Response getStateByPostalCode(@PathParam("id") String id) {
-		return Response.status(200).entity("getStateByPostalCode is called, id : " + id)
+		System.out.println("id: " + id);
+		
+		State state = State.valueOfAbbreviation(id);
+		
+		return Response.status(200).entity("getStateByPostalCode is called, id : " + id + ", state: " + state.name())
 				.build();
 	}
 	
+	@GET
+	@Path("{year}/{month}/{day}")
+	public Response getUserHistory(@PathParam("year") int year,
+			@PathParam("month") int month, @PathParam("day") int day) {
+
+		String date = year + "/" + month + "/" + day;
+
+		return Response.status(200)
+				.entity("getUserHistory is called, year/month/day : " + date)
+				.build();
+
+	}
 	
 }
