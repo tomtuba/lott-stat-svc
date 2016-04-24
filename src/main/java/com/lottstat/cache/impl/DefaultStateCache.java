f29dc342b0187ee638ce8cca8479ab58bc27cf8b
Taylor <tmccomb613@gmail.com>
2016-04-24 12:23:10 -0400

    next step in implementing cache

--- src/main/java/com/lottstat/cache/impl/DefaultStateCache.java
+++ src/main/java/com/lottstat/cache/impl/DefaultStateCache.java
@@ -1,14 +1,36 @@
 package com.lottstat.cache.impl;
 
+import java.util.HashMap;
+
 import com.lottstat.cache.StateCache;
 import com.lottstat.entity.State;
 import com.lottstat.entity.StateEnum;
+import com.lottstat.entity.StateStorage;
 
+// Singleton
 public class DefaultStateCache implements StateCache {
+	private static DefaultStateCache cache;
+	
+	private HashMap<StateEnum, StateStorage> storageBin = new HashMap<StateEnum, StateStorage>();
+	
+	private DefaultStateCache() {
+		
+	}
+	
+	public DefaultStateCache getInstance() {
+		if (cache == null) {
+			cache = new DefaultStateCache();
+		}
+		return cache;
+	}
 
 	@Override
 	public State getState(StateEnum stateEnum) {
-		// TODO Auto-generated method stub
+		StateStorage stateStorage = storageBin.get(stateEnum);
+		
+		if (stateStorage == null) {
+			// we need to get the state in the storage bin
+		}
 		return null;
 	}
 
